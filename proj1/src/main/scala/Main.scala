d34c24f1604a15b28fcb38e5f94603592abdbfe3
Samuel Gruetter <samuel.gruetter@epfl.ch>
2015-08-07 19:11:59 +0200

    grid printing

--- proj1/src/main/scala/Main.scala
+++ proj1/src/main/scala/Main.scala
@@ -1,14 +1,46 @@
 
 import java.awt.BorderLayout
 import java.awt.event.KeyEvent
+import java.io.PrintStream
 import javax.swing.JFrame
 import javax.swing.JLabel
 import javax.swing.SwingConstants
 
+import scala.collection.mutable.ArrayBuffer
+
+
+class HexGrid(val width: Int, val height: Int) {
+  private val grid: ArrayBuffer[ArrayBuffer[Char]] = ArrayBuffer.fill(width)(ArrayBuffer.fill(height)('.'))
+
+  def cell(x: Int, y: Int): Char = grid(x)(y)
+  def setCell(x: Int, y: Int, c: Char): Unit = {
+    grid(x)(y) = c
+  }
+
+  def printTo(ps: PrintStream): Unit = {
+    for (y <- 0 until height) {
+      if (y % 2 == 1) ps.print(" ")
+      for (x <- 0 until width) {
+        ps.print(cell(x,y))
+        ps.print(" ")
+      }
+      ps.println()
+    }
+  }
+}
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("hello")
+    println("hello world")
+
+    val g = new HexGrid(5, 10)
+
+    g.setCell(2, 3, '1')
+    g.setCell(2, 4, '2')
+    g.setCell(2, 5, '3')
+    g.setCell(3, 5, '4')
+
+    g.printTo(System.out)
   }
 }
 
