0745091f5266a3d59e4bad4ac7b97ffde312c489
Samuel Gruetter <samuel.gruetter@epfl.ch>
2015-08-07 18:46:10 +0200

    hello world

---
+++ proj1/build.sbt
@@ -0,0 +1,9 @@
+name := "proj1"
+
+version := "1.0"
+
+scalaVersion := "2.11.5"
+
+libraryDependencies ++= Seq()
+
+scalacOptions ++= Seq("-deprecation", "-feature")
---
+++ proj1/src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+
+import java.awt.BorderLayout
+import java.awt.event.KeyEvent
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.SwingConstants
+
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("hello")
+  }
+}
+
