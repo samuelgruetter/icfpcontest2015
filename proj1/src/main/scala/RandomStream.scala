890cb7510fef5c05713708b31e4b220c8a969ff4
aero-z <aero_super@yahoo.com>
2015-08-07 20:23:29 +0200

    added random stream

---
+++ proj1/src/main/scala/RandomStream.scala
@@ -0,0 +1,14 @@
+
+object RandomStream{
+  val modulusMask = Int.MaxValue
+  val multiplier = 1103515245
+  val increment = 12345
+
+  def apply(seed: Int) = {
+    lazy val s: Stream[Int] = Stream.cons(
+      seed,
+      s.map(x => (x * multiplier + increment) & modulusMask)
+    )
+    s.map(x => (x & 2147418112) >> 16) // get only bits 30..16
+  }
+}
