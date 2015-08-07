d65963412752e2878d3372ded26006cb2d9fc311
Samuel Gruetter <samuel.gruetter@epfl.ch>
2015-08-07 19:33:09 +0200

    use GridCell instead of Char

--- proj1/src/main/scala/Main.scala
+++ proj1/src/main/scala/Main.scala
@@ -3,10 +3,11 @@ import scala.collection.mutable.ArrayBuffer
 
 
 class HexGrid(val width: Int, val height: Int) {
-  private val grid: ArrayBuffer[ArrayBuffer[Char]] = ArrayBuffer.fill(width)(ArrayBuffer.fill(height)('.'))
+  private val grid: ArrayBuffer[ArrayBuffer[GridCell]] =
+    ArrayBuffer.fill(width)(ArrayBuffer.fill(height)(EmptyCell))
 
-  def cell(x: Int, y: Int): Char = grid(x)(y)
-  def setCell(x: Int, y: Int, c: Char): Unit = {
+  def cell(x: Int, y: Int): GridCell = grid(x)(y)
+  def setCell(x: Int, y: Int, c: GridCell): Unit = {
     grid(x)(y) = c
   }
 
@@ -14,7 +15,7 @@ class HexGrid(val width: Int, val height: Int) {
     for (y <- 0 until height) {
       if (y % 2 == 1) ps.print(" ")
       for (x <- 0 until width) {
-        ps.print(cell(x,y))
+        ps.print(cell(x,y).toChar)
         ps.print(" ")
       }
       ps.println()
@@ -22,16 +23,28 @@ class HexGrid(val width: Int, val height: Int) {
   }
 }
 
+sealed abstract class GridCell {
+  def toChar: Char
+}
+
+case object EmptyCell extends GridCell {
+  def toChar = '.'
+}
+case class UnitCell(unitt: Unit) extends GridCell {
+  def toChar = '0'
+}
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("hello world")
 
     val g = new HexGrid(5, 10)
+    val u = UnitCell(())
 
-    g.setCell(2, 3, '1')
-    g.setCell(2, 4, '2')
-    g.setCell(2, 5, '3')
-    g.setCell(3, 5, '4')
+    g.setCell(2, 3, u)
+    g.setCell(2, 4, u)
+    g.setCell(2, 5, u)
+    g.setCell(3, 5, u)
 
     g.printTo(System.out)
 
