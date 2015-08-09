c050616e566a3a9af0614ab0b417cf6d69f64969
Samuel Gruetter <samuel.gruetter@epfl.ch>
2015-08-09 15:51:44 +0200

    first buggy version of GUI

--- proj1/src/main/scala/MainVisualization.scala
+++ proj1/src/main/scala/MainVisualization.scala
@@ -3,6 +3,7 @@ import java.awt.event.{ActionEvent, ActionListener}
 import java.awt.{Frame, Font, BorderLayout}
 import java.io.{PrintStream, ByteArrayOutputStream}
 import javax.swing._
+import javax.swing.event.{CaretEvent, CaretListener}
 
 import Movers._
 
@@ -25,6 +26,8 @@ class Win1 extends JFrame {
     solutions.find(sol => sol.solution.problemId == problemId && sol.solution.seed == seed).get
   }
 
+  def currentSol: VisualizableSolution = strId2Sol(cb.getSelectedItem.asInstanceOf[String])
+
   def run: Unit = {
     initLayout
 
@@ -32,14 +35,24 @@ class Win1 extends JFrame {
 
     for (sol <- solutions) cb.addItem(sol2StrId(sol))
 
-    showStep(solutions.head, 0)
+    commandsField.setText(currentSol.solution.solution)
+    showStep(solutions.head, "")
   }
 
-  def showStep(sol: VisualizableSolution, stepId: Int): Unit = {
-    val grid = new HexGrid(sol.problem.width, sol.problem.height)
-    for (moveChar <- sol.solution.solution.take(stepId)) {
-      val step = Move.fromChar(moveChar).asInstanceOf[Step]
-      grid.move(step.direction)
+  /** commands must have newlines between units */
+  def showStep(sol: VisualizableSolution, commands: String): Unit = {
+    val grid = HexGrid(sol.problem)
+    val cmds = commands.split("\n")
+    val units = RandomStream(sol.solution.seed).take(sol.problem.sourceLength)
+      .map(rand => sol.problem.units(rand % sol.problem.units.length))
+
+    for ((cmd, unit) <- cmds zip units) {
+      grid.spawnUnit(unit)
+      for (moveChar <- cmd.drop(1)) { // drop lock move, newline already dropped by split
+        val step = Move.fromChar(moveChar).asInstanceOf[Step]
+        grid.move(step.direction)
+      }
+      grid.lockUnit()
     }
     val baos = new ByteArrayOutputStream()
     val ps = new PrintStream(baos)
@@ -52,23 +65,53 @@ class Win1 extends JFrame {
 
   var cb: JComboBox[String] = null
   var textArea: JTextArea = null
+  var commandsField: JTextArea = null
+  var posLabel: JLabel = null
 
   def initLayout: Unit = {
     setSize(800, 600)
+    val font = new Font("monospaced", Font.PLAIN, 36)
 
     cb = new JComboBox[String]()
-    val sl = new JSlider()
+    //val sl = new JSlider()
+    commandsField = new JTextArea()
+    //commandsField.setEditable(false)
+    commandsField.setRows(1)
+    commandsField.setAutoscrolls(true)
+    commandsField.setText("line1\nline2\nline number three")
+    commandsField.setFont(font)
+    commandsField.addCaretListener(new CaretListener {
+      override def caretUpdate(caretEvent: CaretEvent): Unit = {
+        val cmds = commandsField.getText(0, caretEvent.getDot)//.replace("\n", "")
+        posLabel.setText(s"Step ${cmds.length - cmds.count(_ == '\n')}")
+        showStep(currentSol, cmds)
+        //println(cmds)
+      }
+    })
+
+    posLabel = new JLabel()
+    posLabel.setFont(font)
+    posLabel.setText("Step 0")
+
     val nav = new JPanel()
     nav.setLayout(new BorderLayout())
 
     nav.add(cb, BorderLayout.WEST)
-    nav.add(sl, BorderLayout.CENTER)
+    nav.add(commandsField, BorderLayout.CENTER)
 
-    val font = new Font("monospaced", Font.PLAIN, 36)
+    val commandsBorder = new JPanel()
+    commandsBorder.setLayout(new BorderLayout())
+    commandsBorder.add(new JScrollPane(commandsField), BorderLayout.PAGE_START)
+    nav.add(commandsBorder, BorderLayout.CENTER)
+    nav.add(posLabel, BorderLayout.EAST)
+
     cb.setFont(font)
     cb.addActionListener(new ActionListener {
       override def actionPerformed(actionEvent: ActionEvent): Unit = {
-        Win1.this.setTitle(Win1.this.getTitle + "hi")
+        //Win1.this.setTitle(Win1.this.getTitle + "hi")
+        commandsField.setText(currentSol.solution.solution)
+        commandsField.setCaretPosition(0)
+        commandsField.requestFocus()
       }
     })
 
@@ -77,7 +120,7 @@ class Win1 extends JFrame {
     textArea.setEditable(false)
 
     setLayout(new BorderLayout)
-    add(nav, BorderLayout.NORTH)
+    add(nav, BorderLayout.PAGE_START)
     add(textArea, BorderLayout.CENTER)
 
     setExtendedState(getExtendedState() | Frame.MAXIMIZED_BOTH)
